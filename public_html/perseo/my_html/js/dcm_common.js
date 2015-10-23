929bc1136584c5c2d67dabdd85dc89fa014e6645
Edgaru10 <edgaru@hotmail.es>
2015-10-23 16:32:13 -0500

    update nucleo

--- public_html/perseo/my_html/js/dcm_common.js
+++ public_html/perseo/my_html/js/dcm_common.js
@@ -3317,6 +3317,35 @@ $(function () {
     })
 })(jQuery);
 
+//inicio nucleo de investigadores>
+$(function () {
+
+    $('.tab-panels .tabs li').on('click', function () {
+
+        var $panel = $(this).closest('.tab-panels');
+
+        $panel.find('.tabs li.active').removeClass('active');
+        $(this).addClass('active');
+
+        //figure out which panel to show
+        var panelToShow = $(this).attr('rel');
+
+        //hide current panel
+        $panel.find('.panel.active').slideUp(800, showNextPanel);
+
+        //show next panel
+        function showNextPanel() {
+            $(this).removeClass('active');
+
+            $('#' + panelToShow).slideDown(800, function () {
+                $(this).addClass('active');
+            });
+        }
+    });
+
+
+});
+
 $(function () {
     $.Metro.initSidebars = function (area) {
         if (area != undefined) {
